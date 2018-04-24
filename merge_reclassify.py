df3f93d704102be654f0688c934ce9264ea7fd24
Kerr D <dk2n16@soton.ac.uk>
2018-04-24 07:24:38 +0100

    Changed gdal_calc to ensure L0 coastline is added to UGM, which does not necessarily match the L0 coastline

--- merge_reclassify.py
+++ merge_reclassify.py
@@ -36,6 +36,31 @@ class Merge:
 			self.save_name = self.raster_wildcard[:-3]
 		else:
 			self.save_name = save_name
+		self.iso_codes = ['AFG', 'ALA', 'ALB', 'DZA', 'ASM', 'AND', 'AGO', 'AIA',\
+		 'ATA', 'ATG', 'ARG', 'ARM', 'ABW', 'AUS', 'AUT', 'AZE', 'BHS', 'BHR', \
+		 'BGD', 'BRB', 'BLR', 'BEL', 'BLZ', 'BEN', 'BMU', 'BTN', 'BOL', 'BES', \
+		 'BIH', 'BWA', 'BVT', 'BRA', 'IOT', 'BRN', 'BGR', 'BFA', 'BDI', 'CPV', \
+		 'KHM', 'CMR', 'CAN', 'CYM', 'CAF', 'TCD', 'CHL', 'CHN', 'COL', 'COM', \
+		 'COG', 'COD', 'COK', 'CRI', 'CIV', 'HRV', 'CUB', 'CUW', 'CYP', 'CZE', \
+		 'DNK', 'DJI', 'DMA', 'DOM', 'ECU', 'EGY', 'SLV', 'GNQ', 'ERI', 'EST', \
+		 'ETH', 'FLK', 'FRO', 'FJI', 'FIN', 'FRA', 'GUF', 'PYF', 'ATF', 'GAB', \
+		 'GMB', 'GEO', 'DEU', 'GHA', 'GIB', 'GRC', 'GRL', 'GRD', 'GLP', 'GUM', \
+		 'GTM', 'GGY', 'GIN', 'GNB', 'GUY', 'HTI', 'HMD', 'VAT', 'HND', 'HKG', \
+		 'HUN', 'ISL', 'IND', 'IDN', 'IRN', 'IRQ', 'IRL', 'IMN', 'ISR', 'ITA', \
+		 'JAM', 'JPN', 'JEY', 'JOR', 'KAZ', 'KEN', 'KIR', 'PRK', 'KOR', 'KWT', \
+		 'KGZ', 'LAO', 'LVA', 'LBN', 'LSO', 'LBR', 'LBY', 'LIE', 'LTU', 'LUX', \
+		 'MAC', 'MKD', 'MDG', 'MWI', 'MYS', 'MDV', 'MLI', 'MLT', 'MHL', 'MTQ', \
+		 'MRT', 'MUS', 'MYT', 'MEX', 'FSM', 'MDA', 'MCO', 'MNG', 'MNE', 'MSR', \
+		 'MAR', 'MOZ', 'MMR', 'NAM', 'NRU', 'NPL', 'NLD', 'NCL', 'NZL', 'NIC', \
+		 'NER', 'NGA', 'NIU', 'NFK', 'MNP', 'NOR', 'OMN', 'PAK', 'PLW', 'PSE', \
+		 'PAN', 'PNG', 'PRY', 'PER', 'PHL', 'PCN', 'POL', 'PRT', 'PRI', 'QAT', \
+		 'REU', 'ROU', 'RUS', 'RWA', 'BLM', 'SHN', 'KNA', 'LCA', 'MAF', 'SPM', \
+		 'VCT', 'WSM', 'SMR', 'STP', 'SAU', 'SEN', 'SRB', 'SYC', 'SLE', 'SGP', \
+		 'SXM', 'SVK', 'SVN', 'SLB', 'SOM', 'ZAF', 'SGS', 'SSD', 'ESP', 'LKA', \
+		 'SDN', 'SUR', 'SJM', 'SWZ', 'SWE', 'CHE', 'SYR', 'TWN', 'TJK', 'TZA', \
+		 'THA', 'TLS', 'TGO', 'TKL', 'TON', 'TTO', 'TUN', 'TUR', 'TKM', 'TCA', \
+		 'TUV', 'UGA', 'UKR', 'ARE', 'GBR', 'UMI', 'USA', 'URY', 'UZB', 'VUT', \
+		 'VEN', 'VNM', 'VGB', 'VIR', 'WLF', 'ESH', 'YEM', 'ZMB', 'ZWE', 'KOS', 'SPR']
 
 	def list_tiffs(self):
 		"""Function to list tiffs for every country in the chosen directory \
@@ -47,13 +72,18 @@ class Merge:
 		Returns:
 		List of absolute filenames."""
 		tiffs = [] #list to return
+		iso_check = [] #append iso's to list to check all iso's accounted for
 		#List the folders in the path
 		list_of_folders = os.listdir(self.path_to_countries)
 		for folder in list_of_folders:
+			iso_check.append(folder[-3:])
 			#List the tiffs in the folders and if wildcard match, append to list.
 			for tif in os.listdir(os.path.join(self.path_to_countries, folder)):
 				if tif.endswith(self.raster_wildcard):
 					tiffs.append(os.path.join(self.path_to_countries, folder, tif))
+		iso_differences = set(self.iso_codes) - set(iso_check) #Find the difference between input folder and all iso's
+		print("There are {0} countries in input and there should be {1}. Missing countries: {2}\
+			".format(len(iso_check), len(self.iso_codes), iso_differences))
 		return tiffs
 
 	def add_tiffs_to_file(self, tiffs):
@@ -100,8 +130,12 @@ class Merge:
 		mastergrid_L0 = r'E:\Merge_script\Merge_UGM\mask_0_ND\ccid100m_0_ND_resamp.tif'
 		out_tif = os.path.join(self.path_to_save_name, "{0}_1_0_ND.tif".format(self.save_name))
 		command = 'C:/Python27/2713/scripts/gdal_calc.py -A {0} -B {1} --outfile={2}\
-		--calc="B*(A==255) + A*(A==1) + A*(A==0)" --NoDataValue=255 --co NUM_THREADS=3\
+		--calc="B*(B==0) + A*(A==1)" --NoDataValue=255 --co NUM_THREADS=3\
 		 --co COMPRESS=LZW --co PREDICTOR=2 --type=Byte'.format(tiff_path, mastergrid_L0, out_tif)
+		##TRY 2 WAYS TO MULTIPLY 
+		#command = 'C:/Python27/2713/scripts/gdal_calc.py -A {0} -B {1} --outfile={2}\
+		#--calc="B*(A==255) + A*(A==1) + A*(A==0)" --NoDataValue=255 --co NUM_THREADS=3\
+		# --co COMPRESS=LZW --co PREDICTOR=2 --type=Byte'.format(tiff_path, mastergrid_L0, out_tif)
 		subprocess.call(command, shell=True)
 
 ######################THIS FUNCTION NOT NEEDED IF GDAL_CALC WORKS WITH VRT########################
@@ -160,29 +194,29 @@ class Merge:
 		print("Binary made")
 
 
-# test_obj = Merge(r'E:\Merge_script\Merge_UGM\datain\WP515640_Global\Raster\Covariates\UGM\2000-2012', "2001.tif", \
-# 	r'E:\Merge_script\Merge_UGM\dataout', "merge_2001")
-# test_obj.test_function()
+test_obj = Merge(r'E:\Merge_script\Merge_UGM\datain\WP515640_Global\Raster\Covariates\UGM\2000-2012', "2001.tif", \
+	r'E:\Merge_script\Merge_UGM\dataout', "merge_2001")
+test_obj.test_function()
 
-# for year in range(2001, 2012, 1):
-# 	start = time.time()
-# 	test_obj = Merge(r'E:\Merge_script\Merge_UGM\datain\WP515640_Global\Raster\Covariates\UGM\2000-2012', "{0}.tif".format(year), \
-# 		r'E:\Merge_script\Merge_UGM\dataout', "merge_{0}".format(year))
-# 	test_obj.test_function()
-# 	end = time.time()
-# 	print(end - start)
+for year in range(2001, 2012, 1):
+	start = time.time()
+	test_obj = Merge(r'E:\Merge_script\Merge_UGM\datain\WP515640_Global\Raster\Covariates\UGM\2000-2012', "{0}.tif".format(year), \
+		r'E:\Merge_script\Merge_UGM\dataout', "merge_{0}".format(year))
+	test_obj.test_function()
+	end = time.time()
+	print(end - start)
 ########################TO DO 19/04 ----> try multiplying VRT with Mastergrid and then extract binary. ############################
 ######################## Try multi ALSO Check NUM_Threads tag#############################
-start = time.time()
-for year in range(2001, 2004, 1):
-	start_in_loop = time.time()
-	first_epoc = Merge(r'Z:\WP515640_Global\Raster\Covariates\UGM\2000-2012', \
-		"{0}.tif".format(year), r'E:\Merge_script\Merge_UGM\dataout', "Urban_{0}".format(year))
-	first_epoc.test_function()
-	end_in_loop = time.time()
-	print ("{0} took {1} minutes".format(year, (end_in_loop - start_in_loop)/60))
-	del start_in_loop, first_epoc, end_in_loop
+# start = time.time()
+# for year in range(2001, 2004, 1):
+# 	start_in_loop = time.time()
+# 	first_epoc = Merge(r'Z:\WP515640_Global\Raster\Covariates\UGM\2000-2012', \
+# 		"{0}.tif".format(year), r'E:\Merge_script\Merge_UGM\dataout', "Urban_{0}".format(year))
+# 	first_epoc.test_function()
+# 	end_in_loop = time.time()
+# 	print ("{0} took {1} minutes".format(year, (end_in_loop - start_in_loop)/60))
+# 	del start_in_loop, first_epoc, end_in_loop
 
-end = time.time()
-print("Processing took {0} minutes".format((end-start)/60))
+# end = time.time()
+# print("Processing took {0} minutes".format((end-start)/60))
 
